00709bcb43b6f2ae82d8f979b33b0ae799db06a3
Shreya-103 <shreyaalpha1@gmail.com>
2026-05-08 10:02:23 +0530

    added dark mode feature

--- calc.js
+++ calc.js
@@ -9,3 +9,26 @@ checkBond.addEventListener("click", ()=>{
 result.innerHTML = `${name1} and ${name2} has ${bond}% bond`; 
     
 });
+let body = document.querySelector('body');
+let container = document.querySelector('.container');
+let input = document.querySelector('input');
+let modeChange = document.querySelector('#mode');
+
+function changeMood(){
+    if(modeChange.innerText === "🌙"){
+        body.style.backgroundColor = '#1e1e2f';
+        container.style.background = '#2b2d42';
+        input.style.backgroundColor = '#edf2f4';
+        checkBond.style.backgroundColor= '#ef233c';
+        result.style.color = '#ffffff';
+        modeChange.innerText = "🔆";
+    }
+    else{
+        body.style.backgroundColor = '#ffffff';
+        container.style.background = 'linear-gradient(135deg, #ff9a9e, #fad0c4)';
+        input.style.backgroundColor = '#ffffff';
+        checkBond.style.backgroundColor= '#593d91';
+        result.style.color = 'black';
+        modeChange.innerText = "🌙";
+    }
+}
